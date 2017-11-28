4ded6eaf21260549130b0f8a6176be2305245b5c
Espressif Systems <wujiangang@espressif.com>
2017-11-28 11:56:36 +0800

    fix(example): Fix IoT_Demo user sector error
    internal gitlab: 4933a9da

--- examples/IoT_Demo/user/user_main.c
+++ examples/IoT_Demo/user/user_main.c
@@ -34,6 +34,8 @@
 #include "user_esp_platform.h"
 #endif
 
+uint32 priv_param_start_sec;
+
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
  * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
@@ -55,30 +57,43 @@ user_rf_cal_sector_set(void)
     switch (size_map) {
         case FLASH_SIZE_4M_MAP_256_256:
             rf_cal_sec = 128 - 5;
+            priv_param_start_sec = 0x3C;
             break;
 
         case FLASH_SIZE_8M_MAP_512_512:
             rf_cal_sec = 256 - 5;
+            priv_param_start_sec = 0x7C;
             break;
 
         case FLASH_SIZE_16M_MAP_512_512:
+            rf_cal_sec = 512 - 5;
+            priv_param_start_sec = 0x7C;
+            break;
         case FLASH_SIZE_16M_MAP_1024_1024:
             rf_cal_sec = 512 - 5;
+            priv_param_start_sec = 0xFC;
             break;
 
         case FLASH_SIZE_32M_MAP_512_512:
+            rf_cal_sec = 1024 - 5;
+            priv_param_start_sec = 0x7C;
+            break;
         case FLASH_SIZE_32M_MAP_1024_1024:
             rf_cal_sec = 1024 - 5;
+            priv_param_start_sec = 0xFC;
             break;
 
         case FLASH_SIZE_64M_MAP_1024_1024:
             rf_cal_sec = 2048 - 5;
+            priv_param_start_sec = 0xFC;
             break;
         case FLASH_SIZE_128M_MAP_1024_1024:
             rf_cal_sec = 4096 - 5;
+            priv_param_start_sec = 0xFC;
             break;
         default:
             rf_cal_sec = 0;
+            priv_param_start_sec = 0;
             break;
     }
 
