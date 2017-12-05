fe32f7f9bd4741d77041b7ff2e10647055f7f47e
Espressif Systems <wujiangang@espressif.com>
2017-12-05 15:06:32 +0800

    fix(api): Fix os_calloc declaration
    Fix the definition error of os_calloc
    
    internal gitlab: 59208741

--- include/mem.h
+++ include/mem.h
@@ -43,10 +43,10 @@ void *pvPortZalloc (size_t sz, const char *, unsigned);
 void *pvPortRealloc (void *p, size_t n, const char *, unsigned);
 
 #ifndef MEMLEAK_DEBUG
-#define MEMLEAK_DEBUG_ENABLE	0
+#define MEMLEAK_DEBUG_ENABLE    0
 #define os_free(s)        vPortFree(s, "", 0)
 #define os_malloc(s)      pvPortMalloc(s, "", 0)
-#define os_calloc(s)      pvPortCalloc(s, "", 0);
+#define os_calloc(l, s)   pvPortCalloc(l, s, "", 0);
 #define os_realloc(p, s)  pvPortRealloc(p, s, "", 0)
 #define os_zalloc(s)      pvPortZalloc(s, "", 0)
 #else
@@ -64,10 +64,10 @@ do{\
 		pvPortMalloc(s, mem_debug_file, __LINE__);	\
 	})
 
-#define os_calloc(s)	\
+#define os_calloc(l, s)	\
 	({	\
 		static const char mem_debug_file[] ICACHE_RODATA_ATTR STORE_ATTR = __FILE__;	\
-		pvPortCalloc(s, mem_debug_file, __LINE__);	\
+		pvPortCalloc(l, s, mem_debug_file, __LINE__);	\
 	})
 
 #define os_realloc(p, s)	\
