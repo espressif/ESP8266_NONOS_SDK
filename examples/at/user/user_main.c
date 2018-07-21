28117a5b23865c10446a946aa1de02863b99cf65
Xu Chun Guang <xuchunguang@espressif.com>
2018-07-21 21:15:48 +0800

    feat(example): Add partition table in examples

--- examples/at/user/user_main.c
+++ examples/at/user/user_main.c
@@ -184,7 +184,7 @@ static const partition_item_t at_partition_table[] = {
 #endif
 };
 
-void user_pre_init(void)
+void ICACHE_FLASH_ATTR user_pre_init(void)
 {
     if(!system_partition_table_regist(at_partition_table, sizeof(at_partition_table)/sizeof(at_partition_table[0]),SPI_FLASH_SIZE_MAP)) {
 		os_printf("system_partition_table_regist fail\r\n");
