19c6d33d6c91a4ba07a91405013fd69443ce055d
hmueller01 <github@euhm.de>
2020-10-29 14:38:06 +0100

    fix mqtt compile warnings, rename DEFAULT_SECURITY to MQTT_SECURITY for better understanding

--- examples/esp_mqtt_proj/mqtt/include/debug.h
+++ examples/esp_mqtt_proj/mqtt/include/debug.h
@@ -11,13 +11,17 @@
 #if defined(GLOBAL_DEBUG_ON)
 #define MQTT_DEBUG_ON
 #endif
+
+#ifndef INFO
 #if defined(MQTT_DEBUG_ON)
 #define INFO( format, ... ) os_printf( format, ## __VA_ARGS__ )
 #else
 #define INFO( format, ... )
 #endif
-// #ifndef INFO
-// #define INFO os_printf
-// #endif
-
+#endif
+/*
+#ifndef INFO
+#define INFO os_printf
+#endif
+*/
 #endif /* USER_DEBUG_H_ */
